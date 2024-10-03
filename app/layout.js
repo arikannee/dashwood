6f4beb59978adc6301e0bf868fde31297274b235
arikannee <nicky_hey@hotmail.com>
2024-10-03 16:45:14 +0100

    add description

--- app/layout.js
+++ app/layout.js
@@ -8,16 +8,18 @@ const montserrat = Montserrat({
 });
 
 export const metadata = {
-  title: "DASHWOOD",
-  description: "Electrical Services",
+  title: "DASHWOOD - Electrical Services",
+  description:
+    "Efficient, safe, and affordable electrical services for residential and commercial properties. At DASHWOOD, we specialize in installations, repairs, and maintenance, ensuring safe and efficient electrical solutions tailored to your needs. Trust our expert electricians to deliver quality workmanship and exceptional service every time.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${montserrat.className} antialiased`}>
-        <Header/>
-        {children}</body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
