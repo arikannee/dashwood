a760d3b55d97254b43734acf59fcd7697ed1058a
arikannee <nicky_hey@hotmail.com>
2024-10-22 16:03:10 +0100

    change /qoute to /quote

--- app/components/header/navbar/Navbar.js
+++ app/components/header/navbar/Navbar.js
@@ -173,7 +173,7 @@ const Navbar = () => {
         >
           <span className="w-56 h-48 rounded bg-gray-800 absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
           <span className="relative w-full text-left transition-colors duration-300 ease-in-out group-hover:text-[#fdec01]">
-            Get A Qoute
+            Get A Quote
           </span>
         </Link>
       </div>
