d0cbf7760001bdd59057bf966c4dfb9a8214d264
arikannee <nicky_hey@hotmail.com>
2024-11-25 10:50:33 +0000

    update social tab

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -4,9 +4,10 @@ import Navbar from "./navbar/Navbar";
 import "./header.css";
 
 import { MdLocationPin } from "react-icons/md";
-import { TbClockHour4 } from "react-icons/tb";
 import { FaPhone } from "react-icons/fa6";
-import { FaChevronDown, FaChevronUp } from "react-icons/fa"; // Icons for arrow up and down
+import { FaChevronDown, FaChevronUp, FaFacebook } from "react-icons/fa"; // Icons for arrow up and down
+import { IoTime } from "react-icons/io5";
+
 
 const SocialTap = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -37,10 +38,10 @@ const SocialTap = () => {
     <div className="header sticky top-0 z-50 shadow-lg">
       {/* Social Tap - Only shows the first line with a dropdown on small screens */}
       <div className={`social-tap ${isVisible ? "block" : "absolute"}`}>
-        <div className="container lg text-gray-600 flex flex-col sm:flex-row text-sm font-medium leading-7 sm:leading-5">
+        <div className="container lg text-gray-600 flex flex-col lg:flex-row text-sm font-medium leading-7 lg:leading-5">
           {/* Single line with dropdown button (visible on small screens) */}
           <div
-            className="flex justify-center items-center sm:hidden cursor-pointer p-1"
+            className="flex justify-center items-center lg:hidden cursor-pointer p-1"
             onClick={toggleDropdown}
           >
             {isExpanded ? <FaChevronUp /> : <FaChevronDown />} {/* Arrow */}
@@ -48,18 +49,25 @@ const SocialTap = () => {
 
           {/* Expanded section with full details */}
           <div
-            className={`flex flex-col grow sm:flex-row ${
-              isExpanded ? "block" : "hidden sm:flex"
-            } justify-around gap-1`}
+            className={`flex flex-col grow lg:flex-row ${
+              isExpanded ? "block" : "hidden lg:flex"
+            } justify-between gap-1`}
           >
             <div className="flex items-center justify-center gap-1">
               <MdLocationPin />
               Richmond, North Yorkshire, DL10
             </div>
             <div className="flex items-center justify-center gap-1">
-              <TbClockHour4 />
-              Mon - Fri 08:00 AM - 04:00 PM
+              <IoTime />
+              Mon - Fri (8:00 AM - 4:00 PM)
             </div>
+            <a
+              className="flex items-center justify-center gap-1"
+              href="https://www.facebook.com/Dashwoodelectrical"
+            >
+              <FaFacebook />
+              Dashwoodelectrical
+            </a>
             <a
               className="flex items-center justify-center gap-1"
               href="tel:+447740272382"
