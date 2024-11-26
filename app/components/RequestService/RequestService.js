bdb21b4e6d0ec0b1c0b6e1dca1eb2b313843b225
arikannee <nicky_hey@hotmail.com>
2024-11-26 11:13:04 +0000

    add honeypot to forms

--- app/components/RequestService/RequestService.js
+++ app/components/RequestService/RequestService.js
@@ -1,3 +1,219 @@
+// "use client";
+// import { useRef, useState } from "react";
+// import emailjs from "emailjs-com";
+// import servicesData from "../../../data/services-data";
+// import { MdElectricBolt, MdExpandMore, MdExpandLess } from "react-icons/md"; // Import additional icons
+
+// export default function RequestService() {
+//   const form = useRef();
+//   const [selectedService, setSelectedService] = useState(""); // Default value is an empty string
+//   const [isDropdownOpen, setIsDropdownOpen] = useState(false); // State for dropdown visibility
+
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+//     emailjs
+//       .sendForm(
+//         "service_es0xemn",
+//         "template_t8h73dq",
+//         form.current,
+//         "dE9jVLT2-NcfIa14z"
+//       )
+//       .then(
+//         (result) => {
+//           alert("Request sent successfully!");
+//           e.target.reset(); // Clear the form on success
+//         },
+//         (error) => {
+//           alert("Failed to send the Request, please try again.");
+//         }
+//       );
+//   };
+
+//   const handleServiceChange = (event) => {
+//     setSelectedService(event.target.value);
+//   };
+
+//   // Function to toggle the dropdown
+//   const toggleDropdown = () => {
+//     setIsDropdownOpen((prev) => !prev);
+//   };
+
+//   return (
+//     <div>
+//       <div className="container -mt-10 z-10 relative text-center lg:text-left">
+//         <div
+//           className="bg-[#fdec01] px-5 py-2 rounded-t-xl text-gray-700 font-bold cursor-pointer inline-flex lg:hidden items-center gap-2"
+//           onClick={toggleDropdown} // Trigger dropdown on click for medium screens and below
+//         >
+//           <MdElectricBolt /> Request Services
+//           {isDropdownOpen ? (
+//             <MdExpandLess className="ml-2" /> // Arrow up when dropdown is open
+//           ) : (
+//             <MdExpandMore className="ml-2" /> // Arrow down when dropdown is closed
+//           )}
+//         </div>
+//         <div className="hidden lg:inline-block bg-[#fdec01] px-5 py-2 rounded-t-xl text-gray-700 font-bold">
+//           <MdElectricBolt className="inline-block" /> Request Services
+//         </div>
+//       </div>
+
+//       {/* Dropdown Content for Medium Screens and Below */}
+//       {isDropdownOpen && (
+//         <div className="bg-gray-600 h-auto py-8 flex justify-center lg:hidden">
+//           <form
+//             ref={form}
+//             onSubmit={sendEmail}
+//             className="w-full rounded-lg flex flex-col lg:flex-row container gap-4"
+//           >
+//             {/* Your Name */}
+//             <div className="w-full lg:w-1/5 lg:mb-0">
+//               <input
+//                 type="text"
+//                 name="name"
+//                 placeholder="Your Name"
+//                 className="block w-full p-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-0"
+//                 required
+//               />
+//             </div>
+
+//             {/* Email and Phone in the same row on small screens */}
+//             <div className="flex w-full lg:w-2/5">
+//               {/* Your Email */}
+//               <div className="w-1/2 pr-2">
+//                 <input
+//                   type="email"
+//                   name="email"
+//                   placeholder="Your Email"
+//                   className="block w-full p-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-0"
+//                   required
+//                 />
+//               </div>
+
+//               {/* Your Phone */}
+//               <div className="w-1/2 pl-2">
+//                 <input
+//                   type="text"
+//                   name="phone"
+//                   placeholder="Your Phone"
+//                   className="block w-full p-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-0"
+//                   required
+//                 />
+//               </div>
+//             </div>
+
+//             {/* Service (Select) */}
+//             <div className="w-full lg:w-1/4">
+//               <select
+//                 name="service"
+//                 value={selectedService} // Use state to control the selected value
+//                 onChange={handleServiceChange}
+//                 className="block w-full p-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-0"
+//                 required
+//               >
+//                 <option value="" disabled>
+//                   Select Service
+//                 </option>
+//                 {/* Default option */}
+//                 {servicesData.map((service, index) => (
+//                   <option key={index} value={service.link}>
+//                     {service.title}
+//                   </option>
+//                 ))}
+//               </select>
+//             </div>
+
+//             {/* Submit Button */}
+//             <div className="w-full lg:w-1/6">
+//               <button
+//                 type="submit"
+//                 className="w-full bg-[#fdec01] text-gray-700 font-semibold py-2 px-4 rounded-lg hover:bg-gray-800 hover:text-[#fdec01]"
+//               >
+//                 Send Request
+//               </button>
+//             </div>
+//           </form>
+//         </div>
+//       )}
+
+//       {/* Form for Large Screens */}
+//       <div className="hidden lg:bg-gray-600 lg:h-auto lg:py-8 lg:flex lg:justify-center">
+//         <form
+//           ref={form}
+//           onSubmit={sendEmail}
+//           className="w-full rounded-lg flex flex-col lg:flex-row container gap-4"
+//         >
+//           {/* Your Name */}
+//           <div className="w-full lg:w-1/5 lg:mb-0">
+//             <input
+//               type="text"
+//               name="name"
+//               placeholder="Your Name *"
+//               className="block w-full p-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-0"
+//               required
+//             />
+//           </div>
+
+//           {/* Email and Phone in the same row on small screens */}
+//           <div className="flex w-full lg:w-2/5">
+//             {/* Your Email */}
+//             <div className="w-1/2 pr-2">
+//               <input
+//                 type="email"
+//                 name="email"
+//                 placeholder="Your Email *"
+//                 className="block w-full p-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-0"
+//                 required
+//               />
+//             </div>
+
+//             {/* Your Phone */}
+//             <div className="w-1/2 pl-2">
+//               <input
+//                 type="text"
+//                 name="phone"
+//                 placeholder="Your Phone"
+//                 className="block w-full p-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-0"
+//                 required
+//               />
+//             </div>
+//           </div>
+
+//           {/* Service (Select) */}
+//           <div className="w-full lg:w-1/4">
+//             <select
+//               name="service"
+//               value={selectedService} // Use state to control the selected value
+//               onChange={handleServiceChange}
+//               className="block w-full p-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-0"
+//               required
+//             >
+//               <option value="" disabled>
+//                 Select Service
+//               </option>
+//               {/* Default option */}
+//               {servicesData.map((service, index) => (
+//                 <option key={index} value={service.link}>
+//                   {service.title}
+//                 </option>
+//               ))}
+//             </select>
+//           </div>
+
+//           {/* Submit Button */}
+//           <div className="w-full lg:w-1/6">
+//             <button
+//               type="submit"
+//               className="w-full bg-[#fdec01] text-gray-700 font-semibold py-2 px-4 rounded-lg hover:bg-gray-800 hover:text-[#fdec01]"
+//             >
+//               Send Request
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
 "use client";
 import { useRef, useState } from "react";
 import emailjs from "emailjs-com";
@@ -8,9 +224,17 @@ export default function RequestService() {
   const form = useRef();
   const [selectedService, setSelectedService] = useState(""); // Default value is an empty string
   const [isDropdownOpen, setIsDropdownOpen] = useState(false); // State for dropdown visibility
+  const [honeypot, setHoneypot] = useState(""); // Honeypot field state
 
   const sendEmail = (e) => {
     e.preventDefault();
+
+    // Honeypot validation: If filled, block submission
+    if (honeypot) {
+      alert("Spam detected. Submission blocked.");
+      return;
+    }
+
     emailjs
       .sendForm(
         "service_es0xemn",
@@ -65,6 +289,15 @@ export default function RequestService() {
             onSubmit={sendEmail}
             className="w-full rounded-lg flex flex-col lg:flex-row container gap-4"
           >
+            {/* Honeypot Field */}
+            <input
+              type="text"
+              name="honeypot"
+              style={{ display: "none" }}
+              value={honeypot}
+              onChange={(e) => setHoneypot(e.target.value)}
+            />
+
             {/* Your Name */}
             <div className="w-full lg:w-1/5 lg:mb-0">
               <input
@@ -142,6 +375,15 @@ export default function RequestService() {
           onSubmit={sendEmail}
           className="w-full rounded-lg flex flex-col lg:flex-row container gap-4"
         >
+          {/* Honeypot Field */}
+          <input
+            type="text"
+            name="honeypot"
+            style={{ display: "none" }}
+            value={honeypot}
+            onChange={(e) => setHoneypot(e.target.value)}
+          />
+
           {/* Your Name */}
           <div className="w-full lg:w-1/5 lg:mb-0">
             <input
--- app/components/contect-home/ContactHome.js
+++ app/components/contect-home/ContactHome.js
@@ -1,9 +1,142 @@
+// "use client";
+// import { useEffect, useState, useRef } from "react";
+// import emailjs from "emailjs-com";
+
+// export default function ContactHome() {
+//   const [isLightImage, setIsLightImage] = useState(false);
+//   const sectionRef = useRef(null);
+
+//   // Set up an intersection observer to detect when the section is in view
+//   useEffect(() => {
+//     const observer = new IntersectionObserver(
+//       ([entry]) => {
+//         // Toggle image based on whether the section is in view
+//         setIsLightImage(entry.isIntersecting);
+//       },
+//       { threshold: 0.5 } // Trigger when at least 50% of the section is visible
+//     );
+
+//     if (sectionRef.current) observer.observe(sectionRef.current);
+
+//     return () => {
+//       // Only unobserve if sectionRef.current exists
+//       if (sectionRef.current) observer.unobserve(sectionRef.current);
+//     };
+//   }, []);
+
+//   // Function to handle form submission
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+
+//     emailjs
+//       .sendForm(
+//         "service_es0xemn", // Replace with your service ID
+//         "template_e9ddum9", // Replace with your template ID
+//         e.target,
+//         "dE9jVLT2-NcfIa14z" // Replace with your user ID
+//       )
+//       .then(
+//         (result) => {
+//           alert("Message sent successfully!");
+//           e.target.reset(); // Clear the form on success
+//         },
+//         (error) => {
+//           alert("Failed to send the message, please try again.");
+//         }
+//       );
+//   };
+
+//   return (
+//     <section id="contact-us" ref={sectionRef} className="bg-gray-800">
+//       <div className="container lg">
+//         <div className="text-center pb-5">
+//           <h1 className="text-lg m-1 text-[#e9d600] font-medium">CONTACT US</h1>
+//           <h2 className="text-3xl text-white leading-tight">
+//             Reach Out for Custom Solutions
+//           </h2>
+//         </div>
+
+//         <div className="flex justify-center items-center">
+//           <div className="mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 lg:px-0 relative">
+//             {/* Contact Form */}
+//             <div className="bg-white p-5 sm:p-10 sm:pb-6 rounded-lg shadow-lg relative">
+//               <form onSubmit={sendEmail}>
+//                 <div className="mb-4">
+//                   <label className="block text-gray-700">Name *</label>
+//                   <input
+//                     type="text"
+//                     name="name"
+//                     className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-0"
+//                     required
+//                   />
+//                 </div>
+//                 <div className="mb-4">
+//                   <label className="block text-gray-700">Email *</label>
+//                   <input
+//                     type="email"
+//                     name="email"
+//                     className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-0"
+//                     required
+//                   />
+//                 </div>
+//                 <div className="mb-4">
+//                   <label className="block text-gray-700">Phone</label>
+//                   <input
+//                     type="tel"
+//                     name="phone"
+//                     className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-0"
+//                     inputMode="numeric"
+//                   />
+//                 </div>
+//                 <div className="mb-4">
+//                   <label className="block text-gray-700">Message *</label>
+//                   <textarea
+//                     name="message"
+//                     className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-0"
+//                     rows="4"
+//                     required
+//                   ></textarea>
+//                 </div>
+//                 <div className="flex justify-center">
+//                   <button
+//                     type="submit"
+//                     className="btn text-gray-600 relative inline-flex items-center justify-center overflow-hidden transition-all rounded-lg hover:bg-[#fdec01] group py-1.5 px-2.5 bg-[#fdec01] font-semibold"
+//                   >
+//                     <span className="w-full h-48 rounded bg-gray-800 absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+//                     <span className="relative w-full text-left transition-colors duration-300 ease-in-out group-hover:text-[#fdec01]">
+//                       Send Message
+//                     </span>
+//                   </button>
+//                 </div>
+//               </form>
+//             </div>
+
+//             {/* Image with Scroll-Triggered Switching */}
+//             <div className="relative h-full flex items-center justify-center ">
+//               <img
+//                 src={
+//                   isLightImage
+//                     ? "/images/yellow-room-light.jpg"
+//                     : "/images/yellow-room-dark.jpg"
+//                 }
+//                 alt="Background with scroll-triggered switching"
+//                 className="rounded-lg transition-opacity duration-1000"
+//               />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+
 "use client";
 import { useEffect, useState, useRef } from "react";
 import emailjs from "emailjs-com";
 
 export default function ContactHome() {
   const [isLightImage, setIsLightImage] = useState(false);
+  const [honeypot, setHoneypot] = useState(""); // Honeypot field state
   const sectionRef = useRef(null);
 
   // Set up an intersection observer to detect when the section is in view
@@ -27,6 +160,12 @@ export default function ContactHome() {
   // Function to handle form submission
   const sendEmail = (e) => {
     e.preventDefault();
+
+    // Honeypot validation: If filled, block submission
+    if (honeypot) {
+      alert("Spam detected. Submission blocked.");
+      return;
+    }
 
     emailjs
       .sendForm(
@@ -61,6 +200,15 @@ export default function ContactHome() {
             {/* Contact Form */}
             <div className="bg-white p-5 sm:p-10 sm:pb-6 rounded-lg shadow-lg relative">
               <form onSubmit={sendEmail}>
+                {/* Honeypot Field */}
+                <input
+                  type="text"
+                  name="honeypot"
+                  style={{ display: "none" }}
+                  value={honeypot}
+                  onChange={(e) => setHoneypot(e.target.value)}
+                />
+
                 <div className="mb-4">
                   <label className="block text-gray-700">Name *</label>
                   <input
