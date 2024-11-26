29154e781878f52b9437eb79cca076e39f74b9ea
arikannee <nicky_hey@hotmail.com>
2024-11-26 11:40:03 +0000

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     siteUrl: 'https://dashwood.online',
     generateRobotsTxt: true,          // Generate robots.txt
+    sitemapSize: 5000, // Optional: Split sitemaps if you have more than 5000 URLs
   };
   
