f317b2226c411c688c3c0c1290b2890e9c109167
arikannee <nicky_hey@hotmail.com>
2025-02-20 14:20:36 +0000

    change projects

--- next.config.js
+++ next.config.js
@@ -1,21 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: "/:path*", // Match all routes
-          has: [
-            {
-              type: "protocol",
-              value: "http", // Detect HTTP
-            },
-          ],
-          destination: "https://dashwood.online/:path*", // Redirect to HTTPS
-          permanent: true, // Set 301 permanent redirect
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
+  async redirects() {
+    return [
+      {
+        source: "/:path*", // Match all routes
+        destination: "https://dashwood.online/:path*", // Redirect to HTTPS
+        permanent: true, // 301 permanent redirect
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
+
   
